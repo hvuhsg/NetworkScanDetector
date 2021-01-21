c6539d68816e9d64cc39fcb5df95b493f1695ffa
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-01-21 22:12:26 +0200

    Automatic find my local ip

--- sniffer.py
+++ sniffer.py
@@ -8,9 +8,29 @@ class Sniffer(Thread):
         super().__init__()
         self.packets = []
         self.packets_queue = packets_queue
+        self.my_ip = None
         self._stop = False
 
+    def setup(self):
+        self.my_ip = self.find_my_ip()
+
+    def find_my_ip(self):
+        src = set()
+        dst = set()
+        while True:
+            packets = sniff(count=10, filter="tcp")
+            for packet in packets:
+                if packet["IP"].src in dst:
+                    return packet["IP"].src
+                else:
+                    src.add(packet["IP"].src)
+                if packet["IP"].dst in src:
+                    return packet["IP"].dst
+                else:
+                    dst.add(packet["IP"].dst)
+
     def run(self):
+        self.setup()
         sniff(
             filter="tcp",
             prn=self.add_packet_to_queue,
@@ -20,7 +40,7 @@ class Sniffer(Thread):
         )
 
     def is_incoming(self, packet):
-        return packet["IP"].dst in ("10.128.0.3", "192.168.1.101", "192.168.1.126")
+        return packet["IP"].dst == self.my_ip
 
     def add_packet_to_queue(self, packet):
         self.packets_queue.put(packet)
